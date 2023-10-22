77873c9b9cf4a71c8a5f2bd9cc913693d566661f
vtabakovic1 <92637655+vtabakovic1@users.noreply.github.com>
2023-10-22 15:57:46 +0200

    Create prvi.java

---
+++ prvi.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Unesite prvi broj: ");
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        System.out.println("Unesite drugi broj: " );
+        int m=scn.nextInt();
+        System.out.println("Prvi broj je: "+n);
+        System.out.println(",a drugi broj je: "+m);
+
+
+    }
+}
